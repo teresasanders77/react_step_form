c4fe1f223b8b5664df3fc9b8e92046bb4acbe259
teresasanders77 <teresasanders77@gmail.com>
2021-02-11 18:39:40 -0700

    initial comit

--- src/components/Confirm.js
+++ src/components/Confirm.js
@@ -4,6 +4,7 @@ import AppBar from '@material-ui/core/AppBar';
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import { List, ListItem, ListItemText } from '@material-ui/core/';
 import Button from '@material-ui/core/Button';
+import emailjs from 'emailjs-com';
 
 export class Confirm extends Component {
   continue = e => {
@@ -16,6 +17,18 @@ export class Confirm extends Component {
     e.preventDefault();
     this.props.prevStep();
   };
+
+  sendEmail = e => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_thog09r', 'template_n5fkwh9', e.target, 'user_gPHNoNvHZXPJbxdR46FjF')
+      .then((result) => {
+        console.log(result.text);
+        this.props.nextStep();
+      }, (error) => {
+        console.log(error.text);
+      });
+  }
 
   render() {
     const {
@@ -30,39 +43,48 @@ export class Confirm extends Component {
             maxWidth='sm'
           >
             <AppBar title="Confirm User Data" />
-            <List>
-              <ListItem>
-                <ListItemText primary="First Name" secondary={firstName} />
-              </ListItem>
-              <ListItem>
-                <ListItemText primary="Last Name" secondary={lastName} />
-              </ListItem>
-              <ListItem>
-                <ListItemText primary="Email" secondary={email} />
-              </ListItem>
-              <ListItem>
-                <ListItemText primary="Occupation" secondary={occupation} />
-              </ListItem>
-              <ListItem>
-                <ListItemText primary="City" secondary={city} />
-              </ListItem>
-              <ListItem>
-                <ListItemText primary="Bio" secondary={bio} />
-              </ListItem>
-            </List>
-            <br />
+            <form onSubmit={this.sendEmail}>
+              <List>
+                <ListItem>
+                  <input type="hidden" className="form-control" name="firstName" value={firstName} />
+                  <ListItemText primary="First Name" secondary={firstName} />
+                </ListItem>
+                <ListItem>
+                  <input type="hidden" className="form-control" name="lastName" value={lastName} />
+                  <ListItemText primary="Last Name" secondary={lastName} />
+                </ListItem>
+                <ListItem>
+                  <input type="hidden" className="form-control" name="email" value={email} />
+                  <ListItemText primary="Email" secondary={email} />
+                </ListItem>
+                <ListItem>
+                  <input type="hidden" className="form-control" name="occupation" value={occupation} />
+                  <ListItemText primary="Occupation" secondary={occupation} />
+                </ListItem>
+                <ListItem>
+                  <input type="hidden" className="form-control" name="city" value={city} />
+                  <ListItemText primary="City" secondary={city} />
+                </ListItem>
+                <ListItem>
+                  <input type="hidden" className="form-control" name="bio" value={bio} />
+                  <ListItemText primary="Bio" secondary={bio} />
+                </ListItem>
+              </List>
+              <br />
 
-            <Button
-              color="secondary"
-              variant="contained"
-              onClick={this.back}
-            >Back</Button>
+              <Button
+                color="secondary"
+                variant="contained"
+                onClick={this.back}
+              >Back</Button>
+              <Button
+                color="primary"
+                variant="contained"
+                type="submit" id="submitBtn"
+              >Confirm & Continue
 
-            <Button
-              color="primary"
-              variant="contained"
-              onClick={this.continue}
-            >Confirm & Continue</Button>
+              </Button>
+            </form>
           </Dialog>
         </>
       </MuiThemeProvider>
